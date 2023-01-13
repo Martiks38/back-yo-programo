9d18935eeaaf01ef9f5c1316e8d89962667e76e4
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-01-13 14:00:41 -0300

    Habilitar dev

--- src/main/java/com/portfolio/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolio/portfolio/PortfolioApplication.java
@@ -18,7 +18,7 @@ public class PortfolioApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/**").allowedOrigins("*");
+                registry.addMapping("/api/v1/**").allowedOrigins("http://localhost:4200");
             }
         };
     }
